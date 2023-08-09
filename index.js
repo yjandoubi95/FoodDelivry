11feace02a2534c464004d5d98aa6b45193ec5db
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-08-09 21:30:08 +0100

    first

---
+++ index.js
@@ -0,0 +1,155 @@
+var pizza=[{
+    type : 'Margerita' ,
+    size : 'S',
+    price : 5000 , 
+    img : ''
+},
+{
+    type : 'Margerita' ,
+    size : 'M',
+    price : 7000 , 
+    img : ''
+},
+{
+type : 'Margherita' ,
+    size : 'L',
+    price : 17000 ,
+    img : '' 
+},
+{
+    type : 'Siciliana' ,
+    size : 'S',
+    price : 6500 , 
+    img : ''
+},
+{
+    type : 'Siciliana' ,
+    size : 'M',
+    price : 9500 , 
+    img : ''
+},
+{
+type : 'Siciliana' ,
+    size : 'L',
+    price : 19000 , 
+    img : ''
+},
+{
+    type : 'Tonno' ,
+    size : 'S',
+    price : 6500 , 
+    img : ''
+},
+{
+    type : 'Tonno' ,
+    size : 'M',
+    price : 9500 , 
+    img : ''
+},
+{
+type : 'Tonno' ,
+    size : 'L',
+    price : 19000 , 
+    img : ''
+},
+{
+    type : 'Regina' ,
+    size : 'S',
+    price : 6500 , 
+    img : ''
+},
+{
+    type : 'Regina' ,
+    size : 'M',
+    price : 9500 , 
+    img : ''
+},
+{
+type : 'Regina' ,
+    size : 'L',
+    price : 19000 , 
+    img : ''
+},
+{
+    type : 'Peperoni' ,
+    size : 'S',
+    price : 8000 , 
+    img : ''
+},
+{
+    type : 'Peperoni' ,
+    size : 'M',
+    price : 11000 , 
+    img : ''
+},
+{
+type : 'Peperoni' ,
+    size : 'L',
+    price : 22000 , 
+    img : ''
+},
+{
+    type : 'Frutti Di Mare' ,
+    size : 'S',
+    price : 16000 , 
+    img : ''
+},
+{
+    type : 'Frutti Di Mare' ,
+    size : 'M',
+    price : 22000 , 
+    img : ''
+},
+{
+    type : 'Frutti Di Mare' ,
+    size : 'L',
+    price : 39000 , 
+    img : ''
+}]
+
+
+var Crepe = [{
+
+    type : 'Sweet crepes',
+    name : 'Nutella', 
+    price : 8000,
+
+},
+{
+    type : 'Sweet crepes',
+    name : 'Nutella and Bananas', 
+    price : 12000
+},
+{
+    type : 'Sweet crepes',
+    name : 'Apple Cinnamon Brown Sugar ', 
+    price : 12000
+}, 
+{
+    type : 'Sweet crepes',
+    name : 'Smores Nutella', 
+    price : 12000
+    
+},  
+{
+    type : 'Savory crepes',
+    name : 'Cheese', 
+    price : 12000
+},
+{
+    type : 'Savory crepes',
+    name : 'Ham and Cheese', 
+    price : 12000
+}, 
+{
+    type : 'Savory crepes',
+    name : 'Chiken and cheese', 
+    price : 12000
+}, 
+{
+    type : 'Savory crepes',
+    name : 'Thon', 
+    price : 12000
+},     
+]
+
